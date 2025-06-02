0569a2d576b8e0b871db115ae55baddd469c4993
Edgar1120 <ealvarezw02@gmail.com>
2025-06-01 22:13:29 -0600

    cambios

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,27 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
+    
     public function register(): void
     {
         //
     }
-
-    /**
-     * Bootstrap any application services.
-     */
+ 
+   
     public function boot(): void
     {
-        //
+        // Fuerza siempre HTTPS en producción
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+
+        Vite::prefetch(concurrency: 3);
     }
 }
